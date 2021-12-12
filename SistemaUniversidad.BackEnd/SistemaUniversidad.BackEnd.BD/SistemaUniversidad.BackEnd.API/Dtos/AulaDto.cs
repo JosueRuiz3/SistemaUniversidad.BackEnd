efec198d485501da06e99f7c1c236e04fd9a63c0
andyR529 <andyjosue123.jr@gmail.com>
2021-12-12 17:37:26 -0600

    e

---
+++ SistemaUniversidad.BackEnd/SistemaUniversidad.BackEnd.BD/SistemaUniversidad.BackEnd.API/Dtos/AulaDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaUniversidad.BackEnd.API.Dtos
+{
+    public class AulaDto
+    {
+        //public AulaDto()
+        //{
+        //    CursosEnAulas = new HashSet<CursosEnAula>();
+        //}
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(10, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string NumeroAula { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public string NombreAula { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public string CreadoPor { get; set; }
+
+        //public virtual ICollection<CursosEnAula> CursosEnAulas { get; set; }
+
+    }
+}
