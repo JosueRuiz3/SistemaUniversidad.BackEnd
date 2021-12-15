6886a46f440f7793d983ee35a4f9d06cfb02e896
JOHANNY JOSE HERNANDEZ CHAVES <johanny.hernandez@ulatina.net>
2021-12-15 17:33:21 -0600

    IMatriculaServices

---
+++ SistemaUniversidad.BackEnd/SistemaUniversidad.BackEnd.BD/SistemaUniversidad.BackEnd.API/Services/Interfaces/IMatriculaServices.cs
@@ -0,0 +1,17 @@
+﻿using SistemaUniversidad.BackEnd.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaUniversidad.BackEnd.API.Services.Interfaces
+{
+   public interface IMatriculaServices
+    {
+        List<Matricula> SeleccionarTodos();
+        Matricula SeleccionarPorId(string id);
+        void Insertar(Matricula model);
+        void Actualizar(Matricula model);
+        void Eliminar(string id);
+    }
+}
