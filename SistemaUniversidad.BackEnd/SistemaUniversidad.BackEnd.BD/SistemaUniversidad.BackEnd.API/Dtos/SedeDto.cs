17c537e16cc5573b09d7bec94d9907b989802411
Josue Ruiz <andyjosue123.jr@gmail.com>
2021-12-15 12:23:44 -0600

    Add files via upload

---
+++ SistemaUniversidad.BackEnd/SistemaUniversidad.BackEnd.BD/SistemaUniversidad.BackEnd.API/Dtos/SedeDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace SistemaUniversidad.BackEnd.API.Dtos
+{
+    public class SedeDto
+    {
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(10, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string CodigoSede { get; set; } 
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public string NombreSede { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+
+        [MaxLength(8, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public string CorreoElectronico { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public string Direccion { get; set; }
+        public bool Activo { get; set; }
+        public string CreadoPor { get; set; }
+    }
+}
