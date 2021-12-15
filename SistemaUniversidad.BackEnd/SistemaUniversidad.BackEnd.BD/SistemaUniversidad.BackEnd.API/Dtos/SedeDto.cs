9b056ffcce8fb0457c88d75fb62f04c7220d8303
andyR529 <andyjosue123.jr@gmail.com>
2021-12-15 13:06:58 -0600

    Arreglos

---
+++ SistemaUniversidad.BackEnd/SistemaUniversidad.BackEnd.BD/SistemaUniversidad.BackEnd.API/Dtos/SedeDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace SistemaUniversidad.BackEnd.API.Models
+{
+    public class SedeDto
+    {
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(10, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string CodigoSede { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public string NombreSede { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public string CorreoElectronico { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        public string Direccion { get; set; }
+
+    }
+}
