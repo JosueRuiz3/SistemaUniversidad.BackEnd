2f082dfb2888b5db0ff5613918fafe1dab3ac52d
JOHANNY JOSE HERNANDEZ CHAVES <johanny.hernandez@ulatina.net>
2021-12-15 17:37:57 -0600

    IProfesoresService

---
+++ SistemaUniversidad.BackEnd/SistemaUniversidad.BackEnd.BD/SistemaUniversidad.BackEnd.API/Services/Interfaces/IProfesoresService.cs
@@ -0,0 +1,17 @@
+﻿using SistemaUniversidad.BackEnd.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaUniversidad.BackEnd.API.Services.Interfaces
+{
+   public interface IProfesoresService
+    {
+        List<Profesore> SeleccionarTodos();
+        Profesore SeleccionarPorId(string id);
+        void Insertar(Profesore model);
+        void Actualizar(Profesore model);
+        void Eliminar(string id);
+    }
+}
