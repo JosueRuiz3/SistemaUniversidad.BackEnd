2d0cc28d3f61cc78da5d726a914f5404f394ca77
JOHANNY JOSE HERNANDEZ CHAVES <johanny.hernandez@ulatina.net>
2021-12-15 12:32:42 -0600

    ICarrerasService

---
+++ SistemaUniversidad.BackEnd/SistemaUniversidad.BackEnd.BD/SistemaUniversidad.BackEnd.API/Services/Interfaces/ICarreraService.cs
@@ -0,0 +1,18 @@
+﻿using SistemaUniversidad.BackEnd.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaUniversidad.BackEnd.API.Services.Interfaces
+{
+   public interface ICarreraService
+    {
+        IEnumerable<Carrera> SeleccionarTodos();
+        Carrera SeleccionarPorId(string id);
+        void Insertar(Carrera model);
+        void Actualizar(Carrera model);
+        void Eliminar(int id);
+
+    }
+}
