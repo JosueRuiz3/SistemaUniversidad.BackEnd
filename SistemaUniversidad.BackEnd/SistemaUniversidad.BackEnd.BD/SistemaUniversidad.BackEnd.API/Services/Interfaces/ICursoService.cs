d83b9937beea2393ccd3c31312f7b25108c6c9a9
JOHANNY JOSE HERNANDEZ CHAVES <johanny.hernandez@ulatina.net>
2021-12-15 17:11:56 -0600

    ICursoService

---
+++ SistemaUniversidad.BackEnd/SistemaUniversidad.BackEnd.BD/SistemaUniversidad.BackEnd.API/Services/Interfaces/ICursoService.cs
@@ -0,0 +1,18 @@
+﻿using SistemaUniversidad.BackEnd.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaUniversidad.BackEnd.API.Services.Interfaces
+{
+    public interface ICursoService
+    {
+        List<Curso> SeleccionarTodos();
+        Curso SeleccionarPorId(string id);
+        void Insertar(Curso model);
+        void Actualizar(Curso model);
+        void Eliminar(string id);
+
+    }
+}
